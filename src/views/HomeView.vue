cb41f738313003711a3a284f2e443c790b7b7db9
FaDoAdamSandler <99231788+FaDoAdamSandler@users.noreply.github.com>
2022-06-24 09:04:19 -0300

    Add files via upload

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+
+    
+</template>
+<style>
+.pedir{
+  margin-left: 1vh;
+}
+.Bottom {
+  height: 10vh;
+}
+.BoxLivro {
+  width: 100%;
+  box-shadow: 0.5px 0.5px 2.5px black;
+  padding: 1rem;
+  margin-bottom: 1vh;
+}
+
+</style>
